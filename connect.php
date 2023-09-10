7295483f8bfd24524f3a2503215e4c93d39079cb
ninamar1e <115149019+ninamar1e@users.noreply.github.com>
2023-09-10 19:03:21 +0800

    first commit

--- connect.php
+++ connect.php
@@ -1,32 +1,21 @@
 <?php
     $firstname = $_POST['firstname'];
-     $middlename = $_POST['middlename'];
-     $lastname = $_POST['lastname'];
-     $birthdate = $_POST['birthdate'];
-     $address = $_POST['address'];
-     $gender = $_POST['gender'];
-     $phone = $_POST['phone'];
-     $email = $_POST['email'];
-     $address = $_POST['address'];
-     $city = $_POST['city'];
-     $school = $_POST['school'];
-     $schooladdress = $_POST['schooladdress'];
-     $lrn = $_POST['lrn'];
-     $grades = $_POST['grades'];
-     $form = $_POST['form'];
-     $picture = $_POST['picture'];
+    $middlename = $_POST['middlename'];
+    $lastname = $_POST['lastname'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $number = $_POST['number']
 
-$ conn = new mysqli('localhost' , 'root' '' '' 'test');
-if($conn->connect_error){
-    die('Connection Fieled :' . $conn->connect_error);
-}else{
-    $stmt = $conn->prepare("insert into registration(firstname, middlename, lastname, address, graduated, schooladdress, emailaddress, grades, form, picture)"
-    values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?));
-    $stmt->bind_param("ssssssssss", $firstname, $middlename, $lastname, $address, $graduated, $schooladdress, $emailaddress, $grades, $form, $picture);
-    $stmt->execute();
-    echo "registration successfully";
-    $stmt->close();
-    $conn->close();
-}
+    $conn = new mysqli('localhost', 'root', '', 'test');
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into reg(firstname, middlename, lastname, gender, email, number)
+            values(?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssssi", $firstname, $middlename, $lastname, $gender, $email, $number);
+        $stmt->execute();
+        echo "Registration Successfully";
+        $stmt->close();
+        $conn->close();
+    }
 ?>
-    
